ca4b70b583d8d4734a404541bee487c9e75d9d2a
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2025-01-10 19:39:08 -0300

    metodos get, post, put e delete

--- src/main/java/com/github/forumhub/domain/topico/DadosDetalharTopico.java
+++ src/main/java/com/github/forumhub/domain/topico/DadosDetalharTopico.java
@@ -4,8 +4,8 @@ import com.github.forumhub.domain.usuario.Usuario;
 
 import java.time.LocalDateTime;
 
-public record DadosDetalharTopico(Long id, String titulo, String mensagem, LocalDateTime dataCriacao, String autor, StatusTopico status) {
+public record DadosDetalharTopico(Long id, String titulo, String mensagem, LocalDateTime dataCriacao, String autor, StatusTopico status, String curso) {
     public DadosDetalharTopico(Topico topico) {
-        this(topico.getId(), topico.getTitulo(), topico.getMensagem(), topico.getDataCriacao(), topico.getAutor(), topico.getStatus());
+        this(topico.getId(), topico.getTitulo(), topico.getMensagem(), topico.getDataCriacao(), topico.getAutor(), topico.getStatus(), topico.getCurso());
     }
 }
